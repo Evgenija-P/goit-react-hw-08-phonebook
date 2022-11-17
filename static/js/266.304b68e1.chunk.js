06aab07bb91d7f87aaa87db9bdf9ff097668cfeb
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2022-11-17 22:44:49 +0000

    Deploying to gh-pages from @ Evgenija-P/goit-react-hw-08-phonebook@fb9048ff9f159848a5bebec28f9a2635b29ac1ff 🚀

--- static/js/266.304b68e1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,o,n){n.r(o),n.d(o,{Home:function(){return h}});var t=n(184),h=function(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:"Welcome to the phone book!"})})};o.default=h}}]);
-//# sourceMappingURL=266.304b68e1.chunk.js.map
